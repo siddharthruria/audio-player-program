76b1017a2f47e010a4b7fd641cacc3cf7ae61ba4
Siddharth Ruria <siddharthruria10@gmail.com>
2024-12-11 16:23:37 +0530

    added all data to the database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const programRoutes = require("./route/program");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api/program", programRoutes);
 
 app.get("/", (_req, res) => {
   res.send("the express server is running here...");
